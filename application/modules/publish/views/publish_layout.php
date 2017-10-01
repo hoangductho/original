1432bb817d9c72f83ddf7b7c4cc78cd6dfe52994
ThoHD <hoangductho.3690@gmail.com>
2017-10-02 01:21:09 +0700

    fix bug facebook shared

--- application/modules/publish/views/publish_layout.php
+++ application/modules/publish/views/publish_layout.php
@@ -16,12 +16,12 @@
     <meta property="fb:app_id" content="550251971759267">
     <meta property="og:site_name" content="Viễn Vọng">
     <meta property="og:type" content="article">
-    <meta property="og:url" content="<?php echo $this->uri->uri_string;?>">
+    <meta property="og:url" content="<?php echo 'https://vienvong.com/'. $this->uri->uri_string;?>">
     <meta property="og:title" content="<?php echo isset($title) ? $title : 'Viễn Vọng'?>">
     <meta property="og:description" content="<?php echo isset($description) ? $description : null; ?>">
     <meta property="og:image" content="<?php echo 'https://vienvong.com' . (isset($image) ? $image : '/assets/images/logo.png'); ?>">
     <!-- <meta property="og:locale:alternate" content="vi_VI"> -->
-    <meta property="article:author" content="https://vienvong.com">	
+    <!-- <meta property="article:author" content="https://vienvong.com">	 -->
 	<!-- Stylesheets -->
 	<link rel="stylesheet" type="text/css" href="/assets/css/supernews/weather.css">	
 	<link rel="stylesheet" type="text/css" href="/assets/css/supernews/style.css">	
