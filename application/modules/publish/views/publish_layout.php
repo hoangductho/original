778ed61d4085a5c2638ee5b3b3e94f9ccb75bf41
ThoHD <hoangductho.3690@gmail.com>
2017-10-10 23:12:07 +0700

    facebook like page

--- application/modules/publish/views/publish_layout.php
+++ application/modules/publish/views/publish_layout.php
@@ -20,8 +20,8 @@
     <meta property="og:title" content="<?php echo isset($title) ? $title : 'Viễn Vọng'?>">
     <meta property="og:description" content="<?php echo isset($description) ? $description : null; ?>">
     <meta property="og:image" content="<?php echo 'https://vienvong.com' . (isset($image) ? $image : '/assets/images/logo.png'); ?>">
-    <!-- <meta property="og:locale:alternate" content="vi_VI"> -->
-    <!-- <meta property="article:author" content="https://vienvong.com">	 -->
+    <meta property="og:locale" content="vi_VI">
+    <meta property="og:author" content="vienvong.com">	
 	<!-- Stylesheets -->
 	<link rel="stylesheet" type="text/css" href="/assets/css/supernews/weather.css">	
 	<link rel="stylesheet" type="text/css" href="/assets/css/supernews/style.css">	
