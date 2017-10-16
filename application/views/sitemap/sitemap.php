37f59506241f1531f26319faa6d820e63f6e54df
ThoHD <hoangductho.3690@gmail.com>
2017-10-17 02:40:01 +0700

    sitemap render fix bug

--- application/views/sitemap/sitemap.php
+++ application/views/sitemap/sitemap.php
@@ -3,14 +3,8 @@
         xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
   <url id="<?php echo 'article_' . $id?>">
     <loc>https://vienvong.com/publish/detail/<?php echo $id?>/<?php echo $friendly?></loc>
-    <news:news>
-      <news:publication>
-        <news:name>Viễn Vọng</news:name>
-        <news:language>vi</news:language>
-      </news:publication>
-      <news:publication_date><?php echo date('Y-m-d\TH:i:sP', strtotime($actived_date));?></news:publication_date>
-      <news:title><?php echo $title?></news:title>
-      <news:keywords><?php echo $keyword?></news:keywords>
-    </news:news>
+    <lastmod><?php echo date('Y-m-d\TH:i:sP', strtotime($actived_date));?></lastmod>
+    <changefreq>always</changefreq>
+    <priority>0.8</priority>
   </url>
 </urlset>
