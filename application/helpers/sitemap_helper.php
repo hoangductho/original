7819a2ff6671fa9e0cf83d6f90a0304b8d809ace
ThoHD <hoangductho.3690@gmail.com>
2017-10-17 02:12:44 +0700

    sitemap render fix bug

--- application/helpers/sitemap_helper.php
+++ application/helpers/sitemap_helper.php
@@ -6,6 +6,26 @@
   * @access    public
   * @param    string
   * @return    string
+  *
+  * @example 
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+      <url>
+        <loc>http://www.example.org/business/article55.html</loc>
+        <news:news>
+          <news:publication>
+            <news:name>The Example Times</news:name>
+            <news:language>en</news:language>
+          </news:publication>
+          <news:genres>PressRelease, Blog</news:genres>
+          <news:publication_date>2008-12-23</news:publication_date>
+          <news:title>Companies A, B in Merger Talks</news:title>
+          <news:keywords>business, merger, acquisition, A, B</news:keywords>
+          <news:stock_tickers>NASDAQ:A, NASDAQ:B</news:stock_tickers>
+        </news:news>
+      </url>
+    </urlset>
   */
   if ( ! function_exists('add_siteindex'))
   {
@@ -16,39 +36,32 @@
 
       // XPath-Querys 
       $parent_path = "urlset"; 
-      $next_path = "id{$article['id']}"; 
-
+      
       // Create a new DOM document 
       $dom = new DOMDocument(); 
+      $dom->validateOnParse = true;
       $dom->load($xml_src); 
 
-      // Find the parent node 
-      // $xpath = new DOMXpath($dom); 
-      // var_dump($xpath);
       // Find parent node 
       $parent = $dom->getElementsByTagName($parent_path); 
 
       // new node will be inserted before this node 
-      $next = $dom->getElementsByTagName($next_path); 
+      $selector = new DOMXPath($dom);
+
+      $next = $selector->query("//*[@id='article_{$article['id']}']");
 
       if($next->length == 0) {
-        // $string = "\n<url>\n\t<loc>https://vienvong.com/publish/detail/{$article['id']}/{$article['friendly']}</loc>\n\t<changefreq>{$article['actived_date']}</changefreq>\n\t<id{$article['id']}>{$article['id']}</id{$article['id']}>\n</url>";
-        
-        // Create the new element 
-        $element = $dom->createElement('url'); 
-        $loc = $dom->createElement('loc', "https://vienvong.com/publish/detail/{$article['id']}/{$article['friendly']}");
-        $freq = $dom->createElement('changefreq', date('Y-m-d h:i:s'));
-        $id = $dom->createElement('id'.$article['id'], $article['id']);
+        $CI = &get_instance();
+        $render = new DOMDocument();
 
-        $element->appendChild($dom->createTextNode("\n\t"));
-        $element->appendChild($loc);
-        $element->appendChild($dom->createTextNode("\n\t"));
-        $element->appendChild($freq);
-        $element->appendChild($dom->createTextNode("\n\t"));
-        $element->appendChild($id);
-        $element->appendChild($dom->createTextNode("\n"));
+        $element = $CI->load->view('sitemap/sitemap', $article, true);
 
-        $element = $dom->importNode($element, true);
+        $render->validateOnParse = true;
+        $render->loadXML($element);
+
+        $element = $render->getElementsByTagName('url'); 
+
+        $element = $dom->importNode($element[0], true);
 
         // Insert the new element 
         $parent->item(0)->appendChild($element); 
---
+++ application/views/sitemap/sitemap.php
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+  <url id="<?php echo 'article_' . $id?>">
+    <loc>https://vienvong.com/publish/detail/<?php echo $id?>/<?php echo $friendly?></loc>
+    <news:news>
+      <news:publication>
+        <news:name>Viễn Vọng</news:name>
+        <news:language>vi</news:language>
+      </news:publication>
+      <news:genres><?php echo $category_name?></news:genres>
+      <news:publication_date><?php echo $actived_date?></news:publication_date>
+      <news:title><?php echo $title?></news:title>
+      <news:keywords><?php echo $keyword?></news:keywords>
+    </news:news>
+  </url>
+</urlset>
