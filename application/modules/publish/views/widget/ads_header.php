00b07c2ababf475fee484816009eea70f88549fe
ThoHD <hoangductho.3690@gmail.com>
2017-10-19 00:12:53 +0700

    add google adsense

---
+++ application/modules/publish/views/widget/ads_header.php
@@ -0,0 +1,9 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- v2_active_header -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-5355896671501389"
+     data-ad-slot="1151004889"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
---
+++ application/modules/publish/views/widget/ads_listpost.php
@@ -0,0 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-fg+56+9c-gk-1g"
+     data-ad-client="ca-pub-5355896671501389"
+     data-ad-slot="7444950490"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
