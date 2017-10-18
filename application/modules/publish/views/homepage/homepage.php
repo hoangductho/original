d0d19369c9cfce39406254696353441042b189d2
ThoHD <hoangductho.3690@gmail.com>
2017-10-18 23:54:05 +0700

    add google adsense

--- application/modules/publish/views/homepage/homepage.php
+++ application/modules/publish/views/homepage/homepage.php
@@ -12,12 +12,16 @@
     <h3 class="section-title">
         <strong>List Articles</strong>
     </h3>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <ins class="adsbygoogle"
          style="display:block"
          data-ad-format="fluid"
          data-ad-layout-key="-fg+56+9c-gk-1g"
          data-ad-client="ca-pub-5355896671501389"
          data-ad-slot="7444950490"></ins>
+    <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
     <?php
         foreach ($articles as $key => $value) {
             $this->view('homepage/quickview_articles', $value);
