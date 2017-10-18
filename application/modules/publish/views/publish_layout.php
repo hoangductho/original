014362fe0c31c4a992071b8067cf2f3fefd8b616
ThoHD <hoangductho.3690@gmail.com>
2017-10-19 00:01:07 +0700

    add google adsense

--- application/modules/publish/views/publish_layout.php
+++ application/modules/publish/views/publish_layout.php
@@ -75,9 +75,17 @@
 				<!-- <h1 class="site-title"><a href="index.html">SuperNews</a></h1> -->
 			</div>
 
-			<!-- <div class="header-ad">
-				<a href="#"><img src="/assets/images/728x90.png" alt=""></a>
-			</div> -->
+			<div class="header-ad">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+				<!-- v2_active_header -->
+				<ins class="adsbygoogle"
+				     style="display:inline-block;width:728px;height:90px"
+				     data-ad-client="ca-pub-5355896671501389"
+				     data-ad-slot="1151004889"></ins>
+				<script>
+				(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
 
 			<div class="clearfix"></div>
 
