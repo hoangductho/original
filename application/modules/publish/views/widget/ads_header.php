41188fc202463d10dda8c1b5908ed08027a1c3a4
ThoHD <hoangductho.3690@gmail.com>
2017-10-19 22:54:42 +0700

    add adsense

--- application/modules/publish/views/widget/ads_header.php
+++ application/modules/publish/views/widget/ads_header.php
@@ -1,8 +1,8 @@
 <!-- v2_active_header -->
-<ins class="adsbygoogle"
+<!-- <ins class="adsbygoogle"
      style="display:inline-block;width:728px;height:90px"
      data-ad-client="ca-pub-5355896671501389"
      data-ad-slot="1151004889"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+</script> -->
