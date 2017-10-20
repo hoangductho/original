2d333235854e022c95f58e06a0ea30b9cb3496a3
ThoHD <hoangductho.3690@gmail.com>
2017-10-20 12:03:46 +0700

    ads accesstrade.vn

---
+++ application/modules/publish/views/widget/content_qc.php
@@ -0,0 +1,3 @@
+<div class="content-qc">
+    <a href='https://pub.accesstrade.vn/deep_link/4712106233689487615?url=http%3A%2F%2Ffptshop.com.vn%2F'> <img src='https://s3-ap-southeast-1.amazonaws.com/images.accesstrade.vn/47d1e990583c9c67424d369f3414728e/5249_970x90_20161118083256602.png'/> </a>
+</div>  
