151fa2aaa0df5805454f44f17e7019542185e9e2
ThoHD <hoangductho.3690@gmail.com>
2017-10-23 19:57:12 +0700

    fix ads listpost

--- application/modules/publish/views/widget/ads_listpost.php
+++ application/modules/publish/views/widget/ads_listpost.php
@@ -1,9 +1,11 @@
-<ins class="adsbygoogle"
+<article id="post-2" class="hentry post clearfix">
+	<ins class="adsbygoogle"
      style="display:block"
      data-ad-format="fluid"
      data-ad-layout-key="-fg+56+9c-gk-1g"
      data-ad-client="ca-pub-5355896671501389"
      data-ad-slot="7444950490"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+	<script>
+	     (adsbygoogle = window.adsbygoogle || []).push({});
+	</script>    
+</article>
